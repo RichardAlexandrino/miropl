60e70b7da68ac39565d888e9efd106bd20544b18
Richard Alexandrino <richardalexandrino@hotmail.com>
2023-07-11 16:05:15 -0300

    Project created

---
+++ src/main/java/com/miropl/mirolp/MirolpApplication.java
@@ -0,0 +1,13 @@
+package com.miropl.mirolp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirolpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirolpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miropl/mirolp/MirolpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miropl.mirolp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirolpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
