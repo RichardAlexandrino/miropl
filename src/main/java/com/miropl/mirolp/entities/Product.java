53a5bfdd61e09c775964410702b05b52eff782b9
Richard Alexandrino <richardalexandrino@hotmail.com>
2023-07-11 19:01:42 -0300

    Product seed

--- src/main/java/com/miropl/mirolp/entities/Product.java
+++ src/main/java/com/miropl/mirolp/entities/Product.java
@@ -2,6 +2,7 @@ package com.miropl.mirolp.entities;
 
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,7 +19,11 @@ public class Product {
 	private String name;
 	private Double quantity;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Product() {
